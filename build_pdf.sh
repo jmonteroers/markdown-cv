54481825fea13403ce89d7b2e18c9b6962f8e6f7
Juan <juan@pxhealthcare.com>
2021-10-04 13:45:58 +0100

    Fix script for name with whitespaces, add margins

--- build_pdf.sh
+++ build_pdf.sh
@@ -2,5 +2,6 @@
 # usage: ./build_pdf.sh <output_filename>
 jekyll build
 cd _site
-wkhtmltopdf --enable-local-file-access index.html $1
+wkhtmltopdf --enable-local-file-access -B 20 -T 20 index.html output.pdf
+mv output.pdf "../${1}"
 cd ..
